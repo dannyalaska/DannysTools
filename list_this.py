55a9b2e30fb70b3087e9f36b07762734031bbd18
Danny McGrory <dannyalaska@mac.com>
2017-05-02 10:37:36 -0400

    fixed list_this to remove empty strings and excess commas at end of list

--- list_this.py
+++ list_this.py
@@ -5,11 +5,12 @@ import pyperclip
 
 def list_no_quotes(mylist):
     # this could be one-lined but is broken up for improved readability
-    mylist = str(mylist.split('\n')).strip('[').strip(']').replace(r'\r', r",").replace(r"'", "")
+    mylist = str(mylist.split('\n')).strip('[').strip(']').replace(r'\r', r",").replace(r"'", "").rstrip(', ')
     pyperclip.copy(mylist)
 
 
 def list_quotes(mylist):
     # this could be one-lined but is broken a little up to improve readability
-    split_on_lines = str(mylist.split('\n')).strip('[').strip(']').replace(r'\r', r"', '")
+    split_on_lines = str(mylist.split('\n')).strip('[').strip(']').replace(r'\r', r"', '").rstrip("''").rstrip(', ')
     pyperclip.copy(split_on_lines)
+
