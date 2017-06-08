3f3812fb78baae3218b43dd2b86b139ba7310489
Danny McGrory <dannyalaska@mac.com>
2017-06-08 16:51:35 -0400

    cleaned up double piping messing up columns

--- cleanup.py
+++ cleanup.py
@@ -4,11 +4,12 @@ import pyperclip
 import re
 import os
 
-sepReg = re.compile(r' +\||\|\s+')
+sepReg = re.compile(r' +\||\|\s+(?!\n)')
 tableReg = re.compile(r'\+?-+\+|->')
 lineReg = re.compile(r'mysql> mysql>?|mysql>')
 endReg = re.compile(r';')
 emptylineReg = re.compile(r'mysql>\n')
+doubles = '||'
 
 
 def clean_up_markup(text):
@@ -17,7 +18,6 @@ def clean_up_markup(text):
     extracted_line = lineReg.search(text)
     extracted_end = endReg.search(text)
     extracted_empty = emptylineReg.search(text)
-
     if extracted_seps:
         text = sepReg.sub('|', text)
     if extracted_table:
@@ -29,7 +29,7 @@ def clean_up_markup(text):
     if extracted_empty:
         text = emptylineReg.sub('\n', text)
 
+    text = text.replace('||', '|-|')
     text = os.linesep.join([s for s in text.splitlines() if s])
 
     pyperclip.copy(text)
-
